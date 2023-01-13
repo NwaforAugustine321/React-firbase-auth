d8f4e0c4e8e2f6e0840f9d64da94623024137a8c
NwaforAugustine321 <nwaforaugustine422@gmail.com>
2023-01-13 10:12:10 +0100

    update

---
+++ build/precache-manifest.8e6362638d65cde686c2fb4714383639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f9b9c18420104e0c227cf2ddebc2ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea35fa2bfddfd0293c2f",
+    "url": "/static/css/2.a63ca25f.chunk.css"
+  },
+  {
+    "revision": "ea35fa2bfddfd0293c2f",
+    "url": "/static/js/2.b01a5e6d.chunk.js"
+  },
+  {
+    "revision": "3c3aad8f92ed7762a955",
+    "url": "/static/js/main.84dd9210.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
